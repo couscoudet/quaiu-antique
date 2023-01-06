2b3fe04e3829721b326c91df257638793d76fcf5
y_sou <ysoultane@gmail.com>
2023-01-06 18:48:14 +0100

    add create_product file

---
+++ src/create_visitor.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "/../bootstrap.php";
+
+$newVisitorEmail = "ysoultane@gmail.com";
+
+$visitor = new Visitor;
+$visitor->setEmail($newVisitorEmail);
+
+$entityManager->persist($visitor);
+$entityManager->flush($visitor);
