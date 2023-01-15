906c566a0caba3ae79fbd57b332042c13debbcc1
y_sou <ysoultane@gmail.com>
2023-01-15 17:19:46 +0100

    use unminified js bundle for bootstrap to deploy

--- lib/MyProject/Views/footer.php
+++ lib/MyProject/Views/footer.php
@@ -1,6 +1,6 @@
 <!-- container end balise -->
 </div>
 
-<script src="/../../../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="/../../../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
 </body>
 </html>
