32fea7cc8d1332bc9624aea65f6ed9bf824ce2f1
y_sou <ysoultane@gmail.com>
2023-01-20 14:04:42 +0100

    clean tree without router

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
+require_once 'bootstrap.php'; 
+require_once __DIR__.'/lib/MyProject/Routes/routeList.php';
 
-// require_once '/../lib/MyProject/Views/header.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/public/assets/custom.css" rel="stylesheet">
+    <title><?php $pageTitle ?></title>
+</head>
+<body>
+<?php
+require_once __DIR__.'/lib/MyProject/Views/header.php';
 
-$prenom = isset($_GET['url']) ? ($_GET['url']) : ('pas de parametre url');
+use MyProject\Route\Router;
 
-echo('hello mon ' . $prenom);?>
+// $router = new Router($_SERVER['REQUEST_URI']);
+// $router->get('/', 'MyProject\Controller\Dishmanager@index');
+// $router->get('/plat/:id', 'MyProject\Controller\Dishmanager@show');
+
+// $router->run();
+
+var_dump($_GET);
+var_dump($_SERVER);
+
+?>
+
+
 <!-- <div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown button
@@ -17,3 +42,6 @@ echo('hello mon ' . $prenom);?>
 </div> -->
 <?php 
 // require_once '../lib/MyProject/Views/footer.php'; ?>
+<script src="/public/assets/bootstrap.bundle.min.js"></script>
+</body>
+</html>
