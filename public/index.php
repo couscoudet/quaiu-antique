183e93b318e395e393fc381885059040a5be157b
Yannick SOULTANE <ysoultane@mediaperf.com>
2023-01-25 16:25:53 +0100

    apache conf from htaccess

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 define("MYPROJECT_DIR", dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'MyProject');
-define("ASSETS", dirname($_SERVER['PHP_SELF']).DIRECTORY_SEPARATOR.'assets');
+define("ASSETS", dirname($_SERVER['SCRIPT_NAME']).DIRECTORY_SEPARATOR.'assets');
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'bootstrap.php'; 
 
@@ -14,7 +14,7 @@ require_once MYPROJECT_DIR.DIRECTORY_SEPARATOR.'Routes'.DIRECTORY_SEPARATOR.'rou
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/public/assets/custom.css" rel="stylesheet">
+    <link href="/assets/custom.css" rel="stylesheet">
     <title><?php $pageTitle ?></title>
 </head>
 <body>
@@ -48,6 +48,6 @@ var_dump(($_SERVER));
 </div> -->
 <?php 
 // require_once '../lib/MyProject/Views/footer.php'; ?>
-<script src="/public/assets/bootstrap.bundle.min.js"></script>
+<script src="/assets/bootstrap.bundle.min.js"></script>
 </body>
 </html>
